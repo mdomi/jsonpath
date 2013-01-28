338a8de12aa4dbcf46d88df6f7b1d341a369e90a
Mike Dominice <mike@mikedominice.com>
2013-01-28 09:51:09 -0500

    added capability for specifying a file to parse as an argument

--- bin/jsonpath.js
+++ bin/jsonpath.js
@@ -3,15 +3,27 @@
  * Licensed under an MIT-style license (see LICENSE for full text)
  */
  /*global require:false, process:false */
-(function () {
+(function (path, fs) {
     'use strict';
 
     var JSONPathStream = require('../lib/jsonpathstream').JSONPathStream,
-        parser = new JSONPathStream(process.argv[2]);
+        parser = new JSONPathStream(process.argv[2]),
+        filename = process.argv[3],
+        readStream;
 
-    process.stdin.pipe(parser);
     parser.pipe(process.stdout);
 
-    process.stdin.resume();
+    if (filename && filename !== '-') {
+        readStream = fs.createReadStream(path.normalize(filename));
+        readStream.on('data', function (data) {
+            parser.write(data);
+        });
+        readStream.on('end', function () {
+            parser.end();
+        });
+    } else {
+        process.stdin.pipe(parser);
+        process.stdin.resume();
+    }
 
-}());
+}(require('path'), require('fs')));
